67d05f6e342a44c2c249305de0ab13f54f725e26
Iipal <malkoleyplay@gmail.com>
2019-12-24 19:51:31 +0200

    added -s flag and choosing branch for sub-project

--- install.sh
+++ install.sh
@@ -1,54 +1,64 @@
 #!/bin/bash
 
-if [ -z "$1" ]; then
-    echo "missed path to folder. (-h for help)"
+if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
+    echo "always     : full path to project what must to be initialized with 'MakeMeBetter'."
+    echo "-h|--help  : print this message."
+    echo "-s|--skip  : skip cloning 'MakeMeBetter' to specified folder."
     exit
 fi
 
-if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
-    echo "1 argument : full path to project what must to be initialized with 'MakeMeBetter'."
-    echo "-h         : print this message."
-    exit
-fi
+function set_project_data {
+    if [ -z "$1" ]; then
+        echo "missed path to folder. (-h for help)"
+        exit
+    fi
+    project_path=$1
+    project_name=$(basename -- $project_path)
+}
+
+list_cloned_repos=
 
 function git_clone {
     echo "Cloning '$1' to -> '$3':"
-    git clone --recurse-submodules $2 cloning_temp_folder
-    if [ -d "./cloning_temp_folder" ]; then
+    git clone --recurse-submodules $2 $4
+    if [ -d "./$4" ]; then
         mkdir -p $3
-        cp -rf cloning_temp_folder/* $3/
-        rm -rf cloning_temp_folder
+        cp -rf $4/* $3/
+        list_cloned_repos+="$4 "
         return 1
     fi
     return 0
 }
 
-project_path=$1
-project_name=$(basename -- $project_path)
-
-mkdir -p $project_path
-
-git_clone MakeMeBetter https://github.com/Iipal/makemebetter $project_path
+if [ "$1" == "-s" ] || [ "$1" == '--skip' ]; then
+    set_project_data $2
+else
+    set_project_data $1
+    git_clone MakeMeBetter https://github.com/Iipal/makemebetter $project_path ./mmb_temp
+    rm -rf $project_path/libs/_example
+fi
 
 clear
 echo ""
-echo " Additional configuration for '$project_name':"
+echo -e " Additional configuration for \e[1m'$project_name'\e[0m:"
 echo ""
 
 function at_exit {
+    rm -rf $list_cloned_repos
     rm -rf $project_path/libs/_example
     exit
 }
 
 function help_message {
-    echo "l: List anything inside the '$project_path'.           | name"
-    echo "a: Add sub-project\library to -> '$project_path/libs'. | url, name"
-    echo "r: Remove library from '$project_path/libs'.           | name"
-    echo "d: Delete '$project_path' and quit.                    | Y/n"
-    echo "h: Print this help message."
-    echo "q: quit."
+    echo -e "\e[36ml\e[39m: List anything inside the '$project_path'           \e[2m| name\e[0m"
+    echo -e "\e[35ma\e[39m: Add sub-project\library to -> '$project_path/libs' \e[2m| url, name\e[0m"
+    echo -e "\e[31mr\e[39m: Remove library from '$project_path/libs'           \e[2m| name\e[0m"
+    echo -e "\e[31md\e[39m: Delete '$project_path' and quit                    \e[2m| Y/n\e[0m"
+    echo -e "\e[34mh\e[39m: Print this help message"
+    echo -e "q: quit"
 }
 
+#git@github.com:Iipal/libtpool
 function add_sub_project {
     subp_url=$1
     if [ -z "$subp_url" ]; then
@@ -68,17 +78,27 @@ function add_sub_project {
         fi
     fi
 
-    git_clone $subp_name $subp_url $project_path/libs/$subp_name
+    git_clone $subp_name $subp_url $project_path/libs/$subp_name ./$subp_name
     if [ "$?" == "0" ]; then
         return 0
     fi
 
+    read -p "Switch to specified branch(Y/n)" is_branch_switch
+    if [ "$is_branch_switch" == "y" ] || [ "$is_branch_switch" == "Y" ]; then
+        git --no-pager -C ./$subp_name branch --remotes
+        read -p "Choose branch(empty to discard):" subp_branch
+        if [ ! -z "$subp_branch" ]; then
+            git -C ./$subp_name checkout $subp_branch
+            cp -rf ./$subp_name/* $project_path/libs/$subp_name/
+        fi
+    fi
+
     echo ""
-    read -p "Initialize this sub-project with 'MakeMeBetter'?(Y/n): " is_customize
-    if [ "$is_customize" == "y" ] || [ "$is_customize" == "Y" ]; then
+    read -p "Initialize this sub-project with 'MakeMeBetter'?(Y/n) " is_mmb_init
+    if [ "$is_mmb_init" == "y" ] || [ "$is_mmb_init" == "Y" ]; then
         mkdir -p "$project_path/libs/$subp_name/configs"
-        cp -rf $project_path/libs/_example/Makefile $project_path/libs/$subp_name/Makefile
-        cp -rf $project_path/libs/_example/configs/* $project_path/libs/$subp_name/configs/
+        cp -rf ./mmb_temp/libs/_example/Makefile $project_path/libs/$subp_name/Makefile
+        cp -rf ./mmb_temp/libs/_example/configs/* $project_path/libs/$subp_name/configs/
     fi
     echo "Done."
 }
@@ -104,12 +124,8 @@ function remove_lib {
     if [ -z "$r_lib" ]; then
         return 0
     elif [ -d "$project_path/libs/$r_lib" ]; then
-        if [ "${r_lib##*/}" == "_example" ]; then
-            echo "** Do not delete '$project_path/libs/_example'. It's will be deleted by me at exit. **"
-        else
-            rm -rf "$project_path/libs/$r_lib"
-            echo "$project_path/libs/$r_lib removed."
-        fi
+        rm -rf "$project_path/libs/$r_lib"
+        echo "$project_path/libs/$r_lib removed."
     else
         echo "$project_path/libs/$r_lib not founded."
     fi
